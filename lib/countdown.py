042503bda43105d3b8a6e0c814d1c729cbdc84e7
Vanessa Jesik <vajesik@gmail.com>
2023-09-18 15:36:15 -0600

    happy new year

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import time
+
+
+def countdown(number):
+    while number > 0:
+        print(f"{number} SECOND(S)!")
+        number -= 1
+    return print("HAPPY NEW YEAR!")
+
+
+def countdown_with_sleep(number):
+    while number > 0:
+        print(f"{number} SECOND(S)!")
+        number -= 1
+        time.sleep(1)
+    return print("HAPPY NEW YEAR!")
